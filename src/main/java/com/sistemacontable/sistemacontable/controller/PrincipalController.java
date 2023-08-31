c75f03d2b3878d976dbf7678db70497e5e05bd90
alexus21 <US21003@ues.edu.sv>
2023-08-31 16:31:51 -0600

    Adding redirecting

--- src/main/java/com/sistemacontable/sistemacontable/controller/PrincipalController.java
+++ src/main/java/com/sistemacontable/sistemacontable/controller/PrincipalController.java
@@ -1,12 +1,21 @@
 package com.sistemacontable.sistemacontable.controller;
 
 import org.springframework.stereotype.Controller;
-import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+import com.sistemacontable.sistemacontable.userdata.User;
+import org.springframework.web.bind.annotation.*;
 
 @Controller
+@RequestMapping("/")
 public class PrincipalController {
-    @RequestMapping(value = "index")
-    public String Principal(){
-        return "index";
+    @GetMapping(value = "/index")
+    public String getIndex(){
+        return "index.html";
+    }
+
+    @PostMapping(value = "/Dashboard")
+    public String Dashboard(@ModelAttribute User user){
+        System.out.println(user.getUsername() + ": " + user.getPassword());
+        return "redirect:/Dashboard.html";
     }
 }
---
+++ src/main/java/com/sistemacontable/sistemacontable/userdata/User.java
@@ -0,0 +1,31 @@
+package com.sistemacontable.sistemacontable.userdata;
+
+public class User {
+
+    public String username;
+    public String password;
+
+    public String getUsername() {
+        return username;
+    }
+
+    public void setUsername(String username) {
+        this.username = username;
+    }
+
+    public String getPassword() {
+        return password;
+    }
+
+    public void setPassword(String password) {
+        this.password = password;
+    }
+
+    @Override
+    public String toString() {
+        return "User{" +
+                "username='" + username + '\'' +
+                ", password='" + password + '\'' +
+                '}';
+    }
+}
