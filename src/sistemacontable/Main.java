1cf5c7be9201f85ada3322a863760545fe1b0f54
angel <angelvasquez2570@gmail.com>
2023-10-04 13:42:51 -0600

    Libro mayor V3

--- src/sistemacontable/Main.java
+++ src/sistemacontable/Main.java
@@ -14,6 +14,7 @@ public class Main {
      * @param args the command line arguments
      */
     public static void main(String[] args) {
+        System.out.println();
         // TODO code application logic here
 
         //<editor-fold defaultstate="collapsed" desc=" Look and feel setting code (optional) ">
